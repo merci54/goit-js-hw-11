e1b7486fc3ec943e43fba611f28428029563458c
Yaroslav <yaroslavlit@gmail.com>
2025-06-13 17:32:20 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+import iziToast from "izitoast";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import "izitoast/dist/css/iziToast.min.css";
