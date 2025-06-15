bdcd3af23ef5faa1063e5b9e5e6181f7f36a4340
Yaroslav <yaroslavlit@gmail.com>
2025-06-16 00:48:49 +0200

    full done

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,48 @@
+
+import axios from "axios";
+
+import { createGallery, showLoader } from "./render-functions";
+
+import { refs } from "./refs";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const API = '50766581-35cab7d84125eeb0dc75e938a';
+const API_URL = 'https://pixabay.com/api/';
+
+
+export function getImagesByQuery(query) {
+
+    // додавання лоадера
+    showLoader();
+
+    // отримання даних 
+    axios(API_URL, {
+        params: {
+            key: API,
+            q: query,
+            image_type: 'photo',
+            orientation: "horizontal"
+        }
+    })
+        .then(({ data }) => {
+            // рендер галереї
+            refs.gallery.innerHTML = createGallery(data.hits);
+
+
+            // якщо не знайшло жодного результату
+            if (data.hits.length === 0) {
+                iziToast.error({
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    position: 'topRight',
+
+                })
+            }
+        })
+        .catch(error => {
+            console.log(error)
+
+        })
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import axios from "axios";
-import iziToast from "izitoast";
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import "izitoast/dist/css/iziToast.min.css";
+import { refs } from "./js/refs";
+import { getImagesByQuery } from './js/pixabay-api'
+import { clearGallery } from "./js/render-functions";
+
+
+refs.form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+    e.preventDefault();
+    // очистка галереї
+    clearGallery();
+
+    // отримання даних та рендер
+    getImagesByQuery(refs.input.value);
+
+}
+
