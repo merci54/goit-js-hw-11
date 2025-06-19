4fd926d6b4745847dc78e5170487cae7c7420da8
Yaroslav <yaroslavlit@gmail.com>
2025-06-19 15:10:28 +0200

    fix main js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,59 +1,24 @@
 
 import axios from "axios";
-
-import { createGallery, showLoader } from "./render-functions";
-
-import { refs } from "./refs";
-
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-
+import { showLoader } from "./render-functions";
 
 const API = '50766581-35cab7d84125eeb0dc75e938a';
-const API_URL = 'https://pixabay.com/api/';
+const API_URL = 'https://pixabay.com/api/e';
 
 
 export function getImagesByQuery(query) {
-
-    if (!query) {
-        // сповіщення для користувачів
-        iziToast.error({
-            message: 'Sorry, there are no images matching your search query. Please try again!',
-            position: 'topRight',
-
-        })
-        return
-    }
-
-    // додавання лоадера
+    //показ лоадеру при початку отримання інформації
     showLoader();
 
-    // отримання даних 
-    axios(API_URL, {
+    // отримання даних
+    return axios(API_URL, {
         params: {
             key: API,
             q: query,
             image_type: 'photo',
-            orientation: "horizontal"
+            orientation: "horizontal",
+            safesearch: 'false',
         }
     })
-        .then(({ data }) => {
-            // рендер галереї
-            refs.gallery.innerHTML = createGallery(data.hits);
 
-
-            // якщо не знайшло жодного результату
-            if (data.hits.length === 0) {
-                // сповіщення для користувачів
-                iziToast.error({
-                    message: 'Sorry, there are no images matching your search query. Please try again!',
-                    position: 'topRight',
-
-                })
-            }
-        })
-        .catch(error => {
-            console.log(error)
-
-        })
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,57 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 import { refs } from "./js/refs";
 import { getImagesByQuery } from './js/pixabay-api'
 import { clearGallery } from "./js/render-functions";
+import { hideLoader } from "./js/render-functions";
+import { createGallery } from "./js/render-functions";
+
 
 
 refs.form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
+
     // очистка галереї
     clearGallery();
 
-    // отримання даних та рендер
-    getImagesByQuery(refs.input.value.trim());
-    console.log(refs.input.value.trim())
+  // перевірка значення інпуту
+  if (!refs.input.value.trim()) {
+    iziToast.error({
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                position: 'topRight',
+
+            })
+    return
+  }
+
+
+  getImagesByQuery(refs.input.value.trim())
+  .then(({ data }) => {
+    // рендер галереї
+    refs.gallery.innerHTML = createGallery(data.hits);
+
+    // якщо не знайшло жодного результату
+    if (data.hits.length === 0) {
+
+        // сповіщення для користувачів
+        iziToast.error({
+            message: 'Sorry, there are no images matching your search query. Please try again!',
+            position: 'topRight',
+        })
+    }})
+    .catch(error => {
+
+    // приховання лоадеру якщо виникла помилка
+    hideLoader();
+    iziToast.error({
+      message: `${error.message}`,
+      position: 'topRight',
+  })
+
+  })
+
 
 }
 
